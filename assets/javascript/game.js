771162d93f42c8cb6a94c5785b9106bbc5ebed9d
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-06-20 15:31:42 -0500

    updates to style.css and game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,60 @@
-// var wins = 0;
-// var losses = 0;
-var guessesRemaining = 10;
-var lettersGuessed = []; //Array to store letters guesses
+document.addEventListener("DOMContentLoaded", () => {
+    "use strict"; // Load DOM Content before running...
 
-// // html id and class vars
-var guessesRemaining_div = document.getElementById("guesses-left"); //div id="guesses-remaining"
-// var lettersGuessed_div = document.getElementById("letters-guessed"); // div id="letters-guessed"
-// var correctLetters_div = document.getElementById("correct-letters"); // div id="correct-letters"
+    // Set global vars and arrays
+    var wins = 0;
+    var losses = 0;
+    var guessesRemaining = 10;
+    var lettersGuessed = []; //Array to store user guesses -- letters only!
+    var incorrectChars; // To store incorrect characters
+    var randomLetter;
+    var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-// const winScore_span = document.getElementById("win-score"); //span id = "win-score"
-// const loseScore_span = document.getElementById("lose-score"); //span id="lose-score"
-// const scoreBoard_div = document.querySelector(".scoreboard");
+    // // HTML id and class vars
+    var guessesRemaining_div = document.getElementById("guesses-left"); //div id="guesses-remaining"
+    var lettersGuessed_div = document.getElementById("letters-guessed"); // div id="letters-guessed"
+    var correctLetters_div = document.getElementById("correct-letters"); // div id="correct-letters"
+    var incorrectCharacters_div = document.getElementById("incorrect-characters"); // div id="incorrect-characters"
+    var winScore_span = document.getElementById("win-score"); //span id = "win-score"
+    var loseScore_span = document.getElementById("lose-score"); //span id="lose-score"
 
-// var userGuess = [];
+    // Action begins
+    var reset = function () {
+        lettersGuessed = [];
+        guessesRemaining = 10;
+    };
 
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+    var pyschicChoice = function () {
+        randomLetter = alphabet[Math.floor(Math.random() * alphabet.length + 1)];
+    };
 
-    const lettersGuessed_div = document.getElementById("letters-guessed");
+    document.onkeyup = function (event) {
+        var userGuess = event.key.toLowerCase();
+        // Update divs here
+        guessesRemaining_div.innerHTML = guessesRemaining;
+        lettersGuessed_div.innerHTML = lettersGuessed.join(" ");
 
-    document.addEventListener('keydown', event => {
-        const key = event.key.toLowerCase();
-        console.log(key); //Make sure key is being logged.
-
-        lettersGuessed.push(key);
-        lettersGuessed_div.innerHTML = ("<span>" + key + "</span>");
-
-        if (guessesRemaining > 0) {
-            // Lower guess remaining count.
-            guessesRemaining_div.innerHTML = guessesRemaining--;
-            // wins++;
+        if (alphabet.includes(userGuess)) {
+            guessesRemaining--;
+            lettersGuessed.push(userGuess);
+            pyschicChoice();
+        } else {
+            incorrectCharacters_div.innerHTML = ("This is not a letter: " + userGuess); // Filter out non alpha characters
         }
-        // document.querySelector("letters-guessed").innerHTML;
-    });
+
+        if (guessesRemaining >= 0) {
+            if (userGuess === randomLetter) {
+                wins++;
+                correctLetters_div.innerHTML = ("You guessed correct! I picked: " + randomLetter);
+                winScore_span.innerHTML = wins;
+                reset();
+            } else if (guessesRemaining == 0) {
+                losses++;
+                loseScore_span.innerHTML = losses;
+                reset();
+            } else
+                console.log("Keep guessing!");
+        }
+    }
+
 });
